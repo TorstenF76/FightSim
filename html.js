0d48163cd477b8514eda2f612bf82684cbe8876b
Torsten Flatter <torsten.flatter@inovex.de>
2014-08-03 23:33:32 +0200

    some more tests

--- html.js
+++ html.js
@@ -1,14 +1,15 @@
 var Html =  {
 	tag: function(name, config, text) {
+		// start tag
 		var html = "<";
 		html += name;
-		
+		// attributes
 		if (config) {
 			$.each(config, function(key, value) {
 				html += " " + key + "=\"" + value + "\"";
 			});
 		}
-		
+		// content and close tag
 		if (text) {
 			html += ">";
 			html += text;
--- htmlTest.js
+++ htmlTest.js
@@ -3,4 +3,7 @@ QUnit.test( "Html.tag", function(assert) {
 	assert.strictEqual( Html.tag("img", {src: "image.png", alt: "my image"}), 
 		"<img src=\"image.png\" alt=\"my image\"/>");
 	assert.strictEqual( Html.tag("b", {}, "bold text"), "<b>bold text</b>");
+	assert.strictEqual( Html.tag("b", {class: "bla"}, "bold text"), "<b class=\"bla\">bold text</b>");
+	assert.strictEqual( Html.tag("div", {class: "bla"}, Html.tag("input", {type: "text"})), 
+		"<div class=\"bla\"><input type=\"text\"/></div>");
 });
