fecd1461529a09eb80ab3efae78de4dec73d2992
Torsten Flatter <torsten.flatter@inovex.de>
2014-08-07 22:29:47 +0200

    console log

--- fightsim.js
+++ fightsim.js
@@ -10,12 +10,14 @@ function garrison(unitsArray) {
 }
 
 function fight(unit1, unit2) {
-	calcFight(unit1, unit2);
-	calcFight(unit2, unit1);
+	console.log(unit1.letter + " and " + unit2.letter + " begin a fight");
+	attack(unit1, unit2);
+	attack(unit2, unit1);
 }
 
-function calcFight(unit1, unit2)
+function attack(unit1, unit2)
 {
+	console.log(unit1.letter + " attacks " + unit2.letter);
 	unit1.hitPointsAfterFight.min -= unit2.damageMax;
 	unit1.hitPointsAfterFight.max -= unit2.damageMin;
 	unit1.hitPointsAfterFight.avg -= ((unit2.damageMax * unit2.accuracy) + (unit2.damageMin * (100-unit2.accuracy))) / 100;
